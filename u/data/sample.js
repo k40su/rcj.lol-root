321ec066484c3b57652b5d15b77e83a0aa3ba976
k40su <kaosu243@gmail.com>
2025-11-24 13:44:37 -0700

    Add files via upload

---
+++ u/data/sample.js
@@ -0,0 +1,16 @@
+var userData = {
+    display: "sample bio",
+    username: "sample",
+    description: "basic ass sample",
+    profilePic: "https://cdn.rcj.lol/bio/i/pfp/sample-01.jpg",
+    location: "rye's basement",
+    occupation: "sample shit",
+    music: [
+        {src: "https://cdn.rcj.lol/bio/mp3/sample-01.mp3", name: "I'm so crazy for youuu </3", artist: "Rebzyyx"},
+        {src: "https://cdn.rcj.lol/bio/mp3/sample-02.mp3", name: "all I want is you", artist: "Rebzyyx"}
+    ],
+    musicActive: true,
+    links: [
+        {icon: 'discord', url: 'https://discord.gg/invite', color: '#7289da'},
+    ],
+};
